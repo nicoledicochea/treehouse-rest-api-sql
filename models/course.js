b6380c6189e40618e64938a52feb606745a9c963
“Nicole <dicocheanicole@gmail.com>
2023-11-06 09:37:14 -0800

    update validation for model attributes

--- models/course.js
+++ models/course.js
@@ -10,16 +10,43 @@ module.exports = (sequelize) => {
         primaryKey: true,
         autoIncrement: true,
       },
-      title: { type: Sequelize.STRING },
-      description: { type: Sequelize.TEXT },
-      estimatedTime: { type: Sequelize.STRING },
-      materialsNeeded: { type: Sequelize.STRING },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "A title is required",
+          },
+          notEmpty: {
+            msg: "Please provide a title",
+          },
+        },
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "A description is required",
+          },
+          notEmpty: {
+            msg: "Please provide a description",
+          },
+        },
+      },
+      estimatedTime: {
+        type: Sequelize.STRING,
+      },
+      materialsNeeded: {
+        type: Sequelize.STRING,
+      },
     },
     { sequelize }
   );
 
   Course.associate = (models) => {
     Course.belongsTo(models.User, {
+      as: "user",
       foreignKey: {
         fieldName: "userId",
         allowNull: false,
--- models/user.js
+++ models/user.js
@@ -10,16 +10,64 @@ module.exports = (sequelize) => {
         primaryKey: true,
         autoIncrement: true,
       },
-      firstName: { type: Sequelize.STRING },
-      lastName: { type: Sequelize.STRING },
-      emailAddress: { type: Sequelize.STRING },
-      password: { type: Sequelize.STRING },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "A first name is required",
+          },
+          notEmpty: {
+            msg: "Please provide a first name",
+          },
+        },
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "A last name is required",
+          },
+          notEmpty: {
+            msg: "Please provide a last name",
+          },
+        },
+      },
+      emailAddress: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: {
+            msg: "The email you entered already exists"
+        },
+        validate: {
+          notNull: {
+            msg: "An email address is required",
+          },
+          isEmail: {
+            msg: "Please provide a valid email address",
+          },
+        },
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "A password is required",
+          },
+          notEmpty: {
+            msg: "Please provide a password",
+          },
+        },
+      },
     },
     { sequelize }
   );
 
   User.associate = (models) => {
     User.hasMany(models.Course, {
+      as: "user",
       foreignKey: {
         fieldName: "userId",
         allowNull: false,
