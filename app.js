1a2107afa3161180dddbabc56c1825124d4862b5
“Nicole <dicocheanicole@gmail.com>
2023-11-06 08:54:40 -0800

    add sequelize auth and sync

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const sequelize = require('./models').sequelize;
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -42,7 +43,21 @@ app.use((err, req, res, next) => {
 // set our port
 app.set('port', process.env.PORT || 5000);
 
+// test the database connection
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.')
+  } catch (error) {
+    console.error(`Unable to connect to the database: ${error}`)
+  }
+})();
+
+// sequelize model sync
 // start listening on our port
-const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
-});
+sequelize.sync({ force: true })
+  .then( () => {
+    const server = app.listen(app.get('port'), () => {
+      console.log(`Express server is listening on port ${server.address().port}`);
+    })    
+  })
