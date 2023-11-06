3dabbd1795faa9061a281df058d6959a19d5bd47
“Nicole <dicocheanicole@gmail.com>
2023-11-06 09:26:02 -0800

    add attributes to user and course models

---
+++ models/course.js
@@ -0,0 +1,31 @@
+"use strict";
+const Sequelize = require("sequelize");
+
+module.exports = (sequelize) => {
+  class Course extends Sequelize.Model {}
+  Course.init(
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: { type: Sequelize.STRING },
+      description: { type: Sequelize.TEXT },
+      estimatedTime: { type: Sequelize.STRING },
+      materialsNeeded: { type: Sequelize.STRING },
+    },
+    { sequelize }
+  );
+
+  Course.associate = (models) => {
+    Course.belongsTo(models.User, {
+      foreignKey: {
+        fieldName: "userId",
+        allowNull: false,
+      },
+    });
+  };
+
+  return Course;
+};
